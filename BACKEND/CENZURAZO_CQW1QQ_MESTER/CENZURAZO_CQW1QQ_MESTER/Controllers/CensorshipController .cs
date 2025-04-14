bf9ab30368523cb099a54a23d627815642fdc3ba
mesternandor04@gmail.com <mesternandor04@gmail.com>
2025-04-14 21:49:29 +0200

    Data_(Api_controller for replacement data)

--- BACKEND/CENZURAZO_CQW1QQ_MESTER/CENZURAZO_CQW1QQ_MESTER/Controllers/CensorshipController .cs
+++ BACKEND/CENZURAZO_CQW1QQ_MESTER/CENZURAZO_CQW1QQ_MESTER/Controllers/CensorshipController .cs
@@ -2,6 +2,7 @@
 using System.Text.RegularExpressions;
 using CENZURAZO_CQW1QQ_MESTER.Models;
 using System.Xml;
+using CENZURAZO_CQW1QQ_MESTER.Data;
 
 namespace CENZURAZO_CQW1QQ_MESTER.Controllers
 {
@@ -9,6 +10,41 @@ namespace CENZURAZO_CQW1QQ_MESTER.Controllers
     [Route("api/[controller]")]
     public class CensorshipController : ControllerBase
     {
+        ICensorRepository repo;
 
+        public CensorshipController(ICensorRepository repo)
+        {
+            this.repo = repo;
+        }
+
+        [HttpGet]
+        public IEnumerable<ReplacementData> GetReplacementData()
+        {
+            return this.repo.Read();
+        }
+
+        [HttpGet("{id}")]
+        public ReplacementData? GetReplacementData(int id)
+        {
+            return this.repo.Read(id);
+        }
+
+        [HttpPost]
+        public void GetReplacementData([FromBody] ReplacementData data)
+        {
+            this.repo.Create(data);
+        }
+
+        [HttpPut]
+        public void EditReplacement([FromBody] ReplacementData data)
+        {
+            this.repo.Update(data);
+        }
+
+        [HttpDelete("{id}")]
+        public void DeleteReplacementdata(int id)
+        {
+            this.repo.Delete(id);
+        }
     }
 }
